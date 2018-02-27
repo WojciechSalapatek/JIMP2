1c115d9f1991e0034823b8567138b3525f851f2c
Wojtek <Wojtek.zarnowka@gmail.com>
2018-02-27 22:53:39 +0100

    Gotowy is_palindrome

---
+++ lab1/palindrome/Palindrome.cpp
@@ -0,0 +1,36 @@
+//
+// Created by mwypych on 02.02.17.
+//
+#include <iostream>
+#include "Palindrome.h"
+
+using std::string;
+using namespace std;
+
+void menu(){
+    int choice = 0;
+    string str;
+    cout<<"Wybierz co chcesz zrobić"<<endl<<"1 - sprawdź palindrom"<<endl<<"2 - wyjście"<<endl;
+    while(choice != 1  && choice != 2) {
+        cin>>choice;
+        cout<<"ello"<<endl;
+    }
+
+    if(choice == 1) {
+        cout<<"Podaj wyraz"<<endl;
+        cin>>str;
+        if(is_palindrome(str)) cout<<"wyraz jest palindromem"<<endl;
+        else cout<<"wyraz nie jest palindromem"<<endl;
+    }
+}
+bool is_palindrome(string str){
+    int length = (int) str.length();
+    for (int i=0;i<length/2;i++){
+        if (str[i] != str[length-i-1]){
+            return false;
+        }
+    }
+    return true;
+
+
+}
