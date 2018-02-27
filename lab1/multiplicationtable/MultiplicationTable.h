cab7961359d024ddf18e90384c8c1d3a69ccaaf0
Wojtek <Wojtek.zarnowka@gmail.com>
2018-02-27 23:42:20 +0100

    Gotowa multiplicationtable

---
+++ lab1/multiplicationtable/MultiplicationTable.h
@@ -0,0 +1,10 @@
+//
+// Created by mwypych on 02.02.17.
+//
+#include <string>
+#ifndef JIMP_EXERCISES_MULTIPLICATIONTABLE_H
+#define JIMP_EXERCISES_MULTIPLICATIONTABLE_H
+
+void MultiplicationTable(int tab[][10]);
+
+#endif //JIMP_EXERCISES_MULTIPLICATIONTABLE_H
