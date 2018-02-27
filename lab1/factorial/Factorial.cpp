e7495b8cc85952a91e14dd1c8584de08d45f2f42
Wojtek <Wojtek.zarnowka@gmail.com>
2018-02-27 20:40:10 +0100

    Factorial improvement

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,5 +4,13 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-  return 0;
+    int wynik = 1;
+    if(value==0) return 1;
+    else if (value > 12 || value < -12) return 0;
+    else if (value <0) {
+        value = -value;
+        if(value %2 !=0) wynik = -1;
+    }
+    for (int i=1;i<=value;i++) wynik *= i;
+  return wynik;
 }
