98a57819c5fc9cc389ce65a9c1c2afe15cdcf35e
Wojciech Sałapatek <salawojc@borg.kis.agh.edu.pl>
2018-03-13 10:32:03 +0100

    new version

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -3,6 +3,18 @@
 //
 #include "Factorial.h"
 
-int factorial(int value) {
-  return 0;
+bool IsOutOfRange(int value);
+
+int Factorial(int value) {
+    if (IsOutOfRange(value)) return 0;
+    int wynik = 1;
+    if(value==0) return 1;
+    if (value <0) {
+        value = -value;
+        if(value %2 !=0) wynik = -1;
+    }
+    for (int i=1;i<=value;i++) wynik *= i;
+  return wynik;
 }
+
+bool IsOutOfRange(int value) { return value > 12 || value < -12; }
