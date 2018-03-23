e281a4137bb75853b82ca0263690d75f945d0ddb
Wojtek <Wojtek.zarnowka@gmail.com>
2018-03-23 17:45:09 +0100

    adds namespace in lab4

--- lab4/netstemplateengine/SimpleTemplateEngine.cpp
+++ lab4/netstemplateengine/SimpleTemplateEngine.cpp
@@ -4,37 +4,38 @@
 
 #include "SimpleTemplateEngine.h"
 using namespace nets;
-nets::View::View(string pat) {
-    pattern = pat;
-}
-
-nets::View::~View() {
-
-}
-
-string View::GetPattern() const {
-    return pattern;
-}
-
-void View::SetPattern(const string &pattern){
-    this->pattern = pattern;
-}
-
-std::string nets::View::Render(const std::unordered_map <std::string, std::string> &model) const {
-    std::smatch matches;
-    std::string result = "";
-    string sketch = View::GetPattern();
-    std::regex sub {"\\{\\{([^\\{\\}]*)\\}\\}"};
-    while(std::regex_search(sketch, matches, sub)){
-        auto find = model.find(matches[1]);
-        if (find != model.end()) {
-            result += matches.prefix().str() + find->second;
-        }
-        else result += matches.prefix().str();
-
-        sketch = matches.suffix().str();
+namespace nets {
+    View::View(string pat) {
+        pattern = pat;
     }
-    result += sketch;
 
-    return result;
+    nets::View::~View() {
+
+    }
+
+    string View::GetPattern() const {
+        return pattern;
+    }
+
+    void View::SetPattern(const string &pattern) {
+        this->pattern = pattern;
+    }
+
+    std::string nets::View::Render(const std::unordered_map<std::string, std::string> &model) const {
+        std::smatch matches;
+        std::string result = "";
+        string sketch = View::GetPattern();
+        std::regex sub{"\\{\\{([^\\{\\}]*)\\}\\}"};
+        while (std::regex_search(sketch, matches, sub)) {
+            auto find = model.find(matches[1]);
+            if (find != model.end()) {
+                result += matches.prefix().str() + find->second;
+            } else result += matches.prefix().str();
+
+            sketch = matches.suffix().str();
+        }
+        result += sketch;
+
+        return result;
+    }
 }
