9bfc48fa869e9b4a39dc03c809748ddde7be0a0c
Wojtek <wojciech.salapatek@gmail.com>
2018-03-30 20:18:45 +0200

    before pull

--- lab5/matrix/Matrix.cpp
+++ lab5/matrix/Matrix.cpp
@@ -3,3 +3,72 @@
 //
 
 #include "Matrix.h"
+#include <ostream>
+#include <iostream>
+
+typedef std::complex<double> complex;
+using std::vector;
+
+
+namespace algebra {
+    Matrix::Matrix(int rows, int cols) {
+        this->rows = rows;
+        this->cols = cols;
+        data = new complex *[rows];
+        for (int i = 0; i < rows; i++) {
+            data[i] = new complex[cols];
+        }
+    }
+
+    Matrix::Matrix(std::initializer_list<vector<complex>>) {
+       /* int  it = 0;
+        int  itc = 0;
+        data = new complex *[elements.size()];
+        for(auto i : elements){
+            data[it] = new complex[i.size()];
+            itc = 0;
+            for(auto j: i) {
+                data[it][itc] = j;
+                itc++;
+            }
+            it++;
+        }
+        rows = it;
+        cols = itc;*/
+    }
+
+    pair<int, int> Matrix::Size() {
+        return {rows, cols};
+    }
+
+    string Matrix::Print() const {
+        string result ="[";
+        std::ostringstream converter;
+        for (int i=0; i< rows; i++){
+            for (int j=0; j<cols; j++) converter<<data[i][j];
+            result += converter.str();
+            converter.str("");
+            result += ", ";
+        }
+        result = result.substr(0,result.length()-2);
+        result += "]";
+        std::cout<<result;
+        return result;
+    }
+
+    Matrix Matrix::Add(Matrix other) const{
+
+    }
+
+    Matrix Matrix::Sub(Matrix other) const {
+
+    }
+
+    Matrix Matrix::Mul(Matrix other) const {
+
+    }
+
+    Matrix Matrix::Pow(int to) const {
+
+    }
+}
--- lab5/matrix/Matrix.h
+++ lab5/matrix/Matrix.h
@@ -5,11 +5,43 @@
 #ifndef JIMP_EXERCISES_MATRIX_H
 #define JIMP_EXERCISES_MATRIX_H
 
+#include <string>
+#include <complex>
+#include <vector>
+using ::std::pair;
 
-class Matrix {
+using std::string;
+using std::vector;
+namespace algebra {
+    class Matrix {
+    public:
+        Matrix();
+
+        Matrix(int rows, int cols);
+
+        Matrix(std::initializer_list<vector<std::complex<double>>>);
+
+        Matrix(string toParse);
+
+        string Print() const;
+
+        Matrix Add(Matrix other) const;
+
+        Matrix Sub(Matrix other) const;
+
+        Matrix Mul(Matrix other) const;
+
+        Matrix Pow(int to) const;
+
+        pair<int,int> Size();
 
 
-};
-
+    private:
+        std::complex<double> **data;
+        int l[5];
+        int rows;
+        int cols;
+    };
+}
 
 #endif //JIMP_EXERCISES_MATRIX_H
